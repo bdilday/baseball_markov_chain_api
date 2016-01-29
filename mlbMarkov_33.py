96bdbcd66b6f9d5609ce2cb3d78048d175d0ae6b
bdilday <ben.dilday.phd@gmail.com>
2016-01-28 19:52:57 -0800

    bar charts

--- mlbMarkov_33.py
+++ mlbMarkov_33.py
@@ -8,25 +8,34 @@ import numpy as np
 import copy
 #from matplotlib.backends.backend_pdf import PdfPages
 from matplotlib import pyplot as plt
+import seaborn as sns
 
 class mlbMarkov:
     def __init__(self, vbose=0, nbases=3, nouts=3):
-
+        self.max_score = 10
         self.vbose = vbose
         self.nbases = nbases
         self.nouts = nouts
         self.state2int = {}
         self.int2state = {}
         self.initEnumerateStates()
+        self.sz = len(self.allStates)
         self.initTransitionMatrix()
         self.probs = {}
-        self.transitionMatrix = []
-        self.valueMatrix = []
         self.probs = {}
         for i in range(10):
             self.probs[i] = 0
         self.init_probs()
         self.solvedSystem = None
+
+        self.transitionMatrix = self.initTransitionMatrix()
+        self.valueMatrix = self.initValueMatrix()
+        self.runsMatrix = np.zeros((self.max_score+1, self.sz))
+        self.makeTransitionMatrix()
+        self.makeValueMatrix()
+        self.summary_keys = ['man1', 'man2', 'man3', 'out0', 'out1', 'out2', 'out3']
+        for i in range(self.max_score+1):
+            self.summary_keys.append('run%d' % i)
 
     def init_probs(self):
         self.probs[1] = 0.15+0.08
@@ -207,6 +216,73 @@ class mlbMarkov:
         for i, v in enumerate(state_vector):
             s = self.int2state[i]
             print '%3d %s %.6f' % (i, s, v)
+
+    def parse_state(self, state, n=0):
+        m3, m2, m1 = state.split('_')[0][:]
+        nouts = int(state.split('_')[1][:])
+        m3 = int(m3)
+        m2 = int(m2)
+        m1 = int(m1)
+        nbase = m3+m2+m1
+        nruns = n - nbase - nouts
+        return {'man3': m3, 'man2': m2, 'man1': m1, 'nouts': nouts, 'nruns': nruns}
+
+    def state_vector_to_summary(self, stateVector, n=0):
+        data = {}
+        for k in self.summary_keys:
+            data[k] = 0
+
+        print data
+        for i, p in enumerate(stateVector):
+            s = self.int2state[i]
+            ans = self.parse_state(s, n=n)
+            print s, ans, p
+            if p==0:
+                continue
+            for k in ['man1', 'man2', 'man3']:
+                data[k] += p*ans[k]
+            assert ans['nruns']>=0 or p<1e-6
+            k = 'run%d' % ans['nruns']
+            data[k] += p
+            k = 'out%d' % ans['nouts']
+            data[k] += p
+
+        return data
+
+    def generate_sequence(self, v0, nseq=10):
+        seq = []
+
+        for i in range(nseq):
+            if i==0:
+                v = copy.copy(v0)
+            else:
+                v = self.transitionMatrix.dot(v)
+
+            summary = self.state_vector_to_summary(v, n=i)
+            print summary
+            seq.append(summary)
+        return seq
+
+    def bar_chart_from_summary(self, summary, n=0, ysc='linear'):
+        xx = []
+        yy = []
+        plt.clf()
+        for i, k in enumerate(self.summary_keys):
+            xx.append(i)
+            yy.append(summary[k])
+
+        plt.bar(np.array(xx)-0.3, yy, color='steelblue')
+        plt.xticks(xx, self.summary_keys, rotation='vertical', fontsize=10)
+        plt.yscale(ysc)
+        plt.ylim(0.01,1)
+        plt.xlim(-0.5, self.max_score+8)
+        plt.text(self.max_score+7, 0.9, 'PA= %03d' % n, ha='right')
+        plt.ylabel('probability')
+        plt.title('baseball markov chain')
+    def make_chart_from_state(self):
+        x = np.zeros(3+3+(self.max_score+1))
+        xx = range(len(x))
+        plt.bar(xx,xx)
 
 
 def main(nbases=3, nouts=3, vbose=0, probs=None):
